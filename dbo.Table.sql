4ab0e9ee3b60426b4c28cdf9d3e0581029169950
AmeyaBhannare <97292443+AmeyaBhannare@users.noreply.github.com>
2022-02-19 18:21:09 +0530

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ProductId] INT NOT NULL PRIMARY KEY ,
+	[ProductName] varchar(50) ,
+	[Rate] Decimal(18,2) ,
+	[Description] varchar(200) ,
+	[CategoryName] varchar(200) 
+
+
+);
+
+insert into [dbo].[Table](ProductId ,
+	[ProductName] ,
+	[Rate] ,
+	[Description] ,
+	[CategoryName] ) values(101,Harry Potter ,10.00,descA ,Books);
